941503b9ecfd4aaf9e2cd3f30c34d637dfedb1bc
VernonNub <Vernonloh6@gmail.com>
2026-06-29 11:34:02 +0800

    RayCast
    RayCast logic has been added into the playermanager script

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -3,13 +3,14 @@ using NUnit.Framework;
 using StarterAssets;
 using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerManager : MonoBehaviour
 {
     [Header("Player Values")]
     public float playerHealth = 100;
     public float playerMaxHealth = 100;
-    public float raycastLength = 3;
+    public float raycastLength = 10;
     private int mask = (1 << 6) | (1 << 7);
     public int playerDeath;
 
@@ -19,14 +20,20 @@ public class PlayerManager : MonoBehaviour
     public List<string> inventory = new List<string>();
 
     [Header("Player Components")]
-    private Camera camera;
+    [SerializeField] Camera playerCamera;
     public CharacterController cc;
     public GameObject checkPoint;
     public StarterAssetsInputs starterAssetsInputs;
 
+    [Header("Collectible & Interactable")]
+    [SerializeField] GameObject collectible;
+    [SerializeField] CollectibleManager collectibleManager;
+    [SerializeField] GameObject interactable;
+    [SerializeField] InteractibleManager interactableManager;
+
     private void OnEnable()
     {
-        camera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        playerCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
         cc = gameObject.GetComponent<CharacterController>();
         starterAssetsInputs = gameObject.GetComponent<StarterAssetsInputs>();
     }
@@ -43,6 +50,8 @@ public class PlayerManager : MonoBehaviour
         {
             HandleDeath();
         }
+
+        HandleRayCast();
     }
 
     private void HandleDeath()
@@ -61,5 +70,46 @@ public class PlayerManager : MonoBehaviour
 
         cc.enabled = false;
         gameObject.transform.position = checkPoint.transform.position;
+    }
+    
+    private void HandleRayCast()
+    {
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, raycastLength, mask))
+        {
+            if(hit.collider.gameObject.layer == 6)
+            {
+                collectible = hit.collider.gameObject;
+            }
+            else if(hit.collider.gameObject.layer == 7)
+            {
+                interactable = hit.collider.gameObject;
+            }
+            
+        }
+        else
+        {
+            collectible = null;
+            interactable = null;
+            collectibleManager = null;
+            interactableManager = null;
+        }
+    }
+
+    private void HandleInteraction()
+    {
+        //Get respective manager script & run interaction
+        if (collectible != null)
+        {
+            collectibleManager = collectible.GetComponent<CollectibleManager>();
+        }
+        else if (interactable != null)
+        {
+            interactableManager = interactable.GetComponent<InteractibleManager>();
+        }
+    }
+
+    void OnInteraction()
+    {
+        HandleInteraction();
     }
 }
